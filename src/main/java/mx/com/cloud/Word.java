48e3b38d0abf61570ffe77a3c0c898138384830e
Juan J. Perea <jjperea@intranet.proconsultingpartners.com>
2018-11-07 11:29:24 -0600

    first commit

---
+++ src/main/java/mx/com/cloud/Word.java
@@ -0,0 +1,24 @@
+package mx.com.cloud;
+
+public class Word {
+
+	public String word;
+
+	public Word() {
+		super();
+	}	
+	
+	public Word(String word) {
+		this();
+		this.word = word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+}
